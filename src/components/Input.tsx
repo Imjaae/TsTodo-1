2e718c64458197aa451e9e7427d61c802ce6203a
Jaeyoung <dlawodud96@naver.com>
2023-01-19 11:21:36 +0900

    chnageevent type 적용

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,34 +1,34 @@
-import React, { useState, Dispatch, SetStateAction } from "react";
+import React, { useState, Dispatch, SetStateAction, ChangeEvent } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { uuid } from "uuidv4";
 
 // // useState를 props해 가져오기 위한 첫번째 방법 : 이건 아닌것 같긴함..
-// interface States {
-//   setTodos: Isettodos;
-// }
+interface States {
+  setTodos: Isettodos;
+}
 
-// interface Isettodos {
-//   title: string;
-//   contents: string;
-//   isDone: false;
-//   id: myuuid;
-
-// // uuidv4() 의 type 설정하는 방법
-// interface myuuid {
-//   uuidv4(): string;
-// }
+interface Isettodos {
+  title: string;
+  contents: string;
+  isDone: false;
+  id: myuuid;
+}
+// uuidv4() 의 type 설정하는 방법
+interface myuuid {
+  uuidv4(): string;
+}
 
 // useState를 props해 가져오기 위한 두번째 방법 : 이것이 깔끔하고 에러가 없긴한데 맞는지는 잘 모르겠음...
 interface Props {
   todos: boolean;
-  setTodos: Dispatch<SetStateAction<boolean>>;
+  setTodos: Dispatch<SetStateAction<object[]>>;
 }
 
 function Input({ setTodos, todos }: Props) {
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
 
-  const handleSubmitClick = (event) => {
+  const handleSubmitClick = (event: ChangeEvent<HTMLFormElement>) => {
     console.log(todos);
     event.preventDefault();
 
@@ -44,10 +44,10 @@ function Input({ setTodos, todos }: Props) {
     });
   };
 
-  const handleTitleChange = (event) => {
+  const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
   };
-  const handleContentsChange = (event) => {
+  const handleContentsChange = (event: ChangeEvent<HTMLInputElement>) => {
     setContents(event.target.value);
   };
 
